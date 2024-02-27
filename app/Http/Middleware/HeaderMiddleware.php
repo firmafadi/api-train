935e86f7f7661af1dc1d9d9e397e1aaf3b590968
firmafadi <firmafirmansyah@gmail.com>
2024-02-27 22:55:17 +0700

    add HEADER

--- app/Http/Middleware/HeaderMiddleware.php
+++ app/Http/Middleware/HeaderMiddleware.php
@@ -20,9 +20,54 @@ class HeaderMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if ($request->header('X-TIMESTAMP') && $request->header('X-TIMESTAMP') == date('Y-m-d')) {
-            return $next($request);
+            if ($request->header('X-SIGNATURE')) {
+                $granted = $this->isGranted($request);
+                if (!$granted['status']) {
+                    return $this->setResponse(null, 400, $granted['message']);
+                }
+                return $next($request);
+            }else{
+                return $this->setResponse(null, 400, "Invalid header SIGNATURE");
+            }
+        }else{
+            return $this->setResponse(null, 400, "Invalid header TIMESTAMP");
+        }
+    }
+
+    public function isGranted($req)
+    {
+        if (!$req->header('X-MERCHANT')) {
+            return [
+                'status'=>false,
+                'message'=>'Invalid header MERCHANT'
+            ];
         }
 
-        return $this->setResponse(null, 400, "Invalid header request");
+        $signature = $req->header('X-SIGNATURE');
+        $sign = explode(':', base64_decode($signature));
+        // dd($sign);
+        if ($sign[0] != 'employee') {
+            return [
+                'status'=>false,
+                'message'=>'Invalid header SIGNATURE[1]'
+            ];
+        }
+        if ($sign[1] != date('Y-m-d')) {
+            return [
+                'status'=>false,
+                'message'=>'Invalid header SIGNATURE[2]'
+            ];
+        }
+        if ($sign[2] != $req->header('X-MERCHANT')) {
+            return [
+                'status'=>false,
+                'message'=>'Invalid header SIGNATURE[3]'
+            ];
+        }
+
+        return [
+            'status'=>true,
+            'message'=>'sukses'
+        ];
     }
 }
--- app/Http/Middleware/HeaderMiddlewareDate.php
+++ app/Http/Middleware/HeaderMiddlewareDate.php
@@ -19,11 +19,56 @@ class HeaderMiddlewareDate
 
     public function handle(Request $request, Closure $next): Response
     {
-        // dd(date('d/m/Y'));
         if ($request->header('X-TIMESTAMP') && $request->header('X-TIMESTAMP') == date('d/m/Y')) {
-            return $next($request);
+            if ($request->header('X-SIGNATURE')) {
+                $granted = $this->isGranted($request);
+                if (!$granted['status']) {
+                    return $this->setResponse(null, 400, $granted['message']);
+                }
+                return $next($request);
+            }else{
+                return $this->setResponse(null, 400, "Invalid header SIGNATURE");
+            }
+        }else{
+            return $this->setResponse(null, 400, "Invalid header TIMESTAMP");
+        }
+        
+    }
+
+    public function isGranted($req)
+    {
+        if (!$req->header('X-MERCHANT')) {
+            return [
+                'status'=>false,
+                'message'=>'Invalid header MERCHANT'
+            ];
         }
 
-        return $this->setResponse(null, 400, "Invalid header request");
+        $signature = $req->header('X-SIGNATURE');
+        $sign = explode(':', base64_decode($signature));
+        // dd($sign[0].'='.date('d/m/Y'));
+        if ($sign[0] != date('d/m/Y')) {
+            return [
+                'status'=>false,
+                'message'=>'Invalid header SIGNATURE[1]'
+            ];
+        }
+        if ($sign[1] != 'student') {
+            return [
+                'status'=>false,
+                'message'=>'Invalid header SIGNATURE[2]'
+            ];
+        }
+        if ($sign[2] != $req->header('X-MERCHANT')) {
+            return [
+                'status'=>false,
+                'message'=>'Invalid header SIGNATURE[3]'
+            ];
+        }
+
+        return [
+            'status'=>true,
+            'message'=>'sukses'
+        ];
     }
 }
